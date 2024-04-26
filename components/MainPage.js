6fe93827996544e78679e51c1e2996d81b44cf7d
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-04-26 15:06:10 +0200

    Initial commit

---
+++ components/MainPage.js
@@ -0,0 +1,15 @@
+app.component('main-page', {
+    template: /*HTML*/ `
+    
+        <div class="MainPage-Container">
+            <div class="MainPageSlide">
+                <div class="MainPageSlide-Textbox">
+                    <h1>test <br/> test <br/> test</h1>
+                    <div>test</div>
+                </div>
+                <img src="assets/GOAT2.png" class="MainPageSlide-Imgbox">
+            </div>
+        </div>
+    
+    `,
+}) 
---
+++ components/NavBar.js
@@ -0,0 +1,23 @@
+app.component('nav-bar', {
+    template: /*HTML*/ `
+    
+        <div class="Navbar-Container">
+            <img src="assets/GOAT2.png"  class="Navbar-Icon">
+            <div class="Navbar-Button">Side 1</div>
+            <div class="Navbar-Button">Side 2</div>
+            <div class="Navbar-Button">Side 3</div>
+        </div>
+    
+    
+    `,
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    }
+})
